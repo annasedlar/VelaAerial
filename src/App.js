66d0b41a51eab2f9d22ec2ee75d18204ce490116
Anna Sedlar <annasedlar@gmail.com>
2017-08-14 21:14:17 -0400

    drone icon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './drone-bleu.png';
 import './App.css';
 
 class App extends Component {
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>VelaAerial</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Welcome to this site. A web site. a Frank site. 
         </p>
       </div>
     );
